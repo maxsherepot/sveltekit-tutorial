a73488783c091a52cebe984334c461b8f48934c8
Max <maksym@smartrr.com>
2023-11-25 23:45:23 +0200

    icons section template added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
-<h1 class="text-3xl font-bold">Hello world!</h1>
+<script>
+</script>
 
+<div class="container mx-auto p-5 border border-gray-500 mt-5">
+    <div class="grid md:grid-cols-2 gap-5">
+        <div class="text-center">
+            <div class="flex gap-5 mb-5">
+                <div
+                    class="bg-neutral-700 flex-1 py-5 cursor-pointer hover:ring-2 text-white"
+                >
+                    Content Creator
+                </div>
+                <div
+                    class="bg-neutral-500 flex-1 py-5 cursor-pointer hover:ring-2 text-white"
+                >
+                    Admins
+                </div>
+            </div>
 
+            <div
+                class="bg-neutral-400 mb-5 py-5 cursor-pointer hover:ring-2 text-white"
+            >
+                Publications
+            </div>
+
+            <div class="flex gap-5">
+                <div
+                    class="bg-neutral-200 w-2/3 py-5 cursor-pointer hover:ring-2 border"
+                >
+                    Chanels & Templates
+                </div>
+                <div
+                    class="bg-neutral-100 w-1/3 py-5 cursor-pointer hover:ring-2 border"
+                >
+                    Chatbox
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-neutral-200">Scheduled Publications</div>
+    </div>
+</div>
